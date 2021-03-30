a9493ef6880b525acf2389791c3914389753dd07
meno1011 <39692905+meno1011@users.noreply.github.com>
2021-03-31 00:23:11 +0900

    #3.4 Methods part Two and instance and property

--- main.py
+++ main.py
@@ -4,11 +4,25 @@ class Car():
     windows = 4
     seats = 4
     
-    def start(potato):
-        #method
-        print(potato.color)
-        print("I started")
+#print(dir(Car))
+#dir()은 모든 property를 보여준다
 
-porche = Car()
-porche.color = "Red Apple Red"
-porche.start()
+    def __init__(self, *args, **kwargs):
+        print(kwargs)
+        #kwargs의 구조는 dictionary이다 key , value로 get을 이용해서 값을 가져올수 있다.
+        self.wheels = 4
+        self.doors = 4
+        self.windows = 4
+        self.seats = 4
+        self.color = kwargs.get("color","black")
+        self.price = kwargs.get("price","$20")
+    
+    #method를 보여주기 class 안에 tab으로 구분해서 들어가있으면 method가 된다.
+    def __str__(self):
+        return f"Car with {self.wheels} wheels"
+
+porche = Car(color = "GREEN", price = "$40")
+print(porche.color , porche.price)
+
+mini = Car()
+print(mini.color, mini.price)
